a06e4e63a16b11cb80d64764cf8922fa959e57c0
muzammil <muzammil@muzammils-MacBook-Pro.local>
2023-02-14 20:18:28 +0500

    wrong [ADD]

--- src/pages/MainScreen/MainScreen.js
+++ src/pages/MainScreen/MainScreen.js
@@ -12,6 +12,7 @@ import {
   Image
 } from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import AntDesign from 'react-native-vector-icons/AntDesign'
 import Colors from '../../styles/Colors';
 
 import { styles } from './styles';
@@ -19,6 +20,11 @@ import { styles } from './styles';
 const MainScreen = ({ navigation, route }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.wrongCodeContainer}>
+        <Image
+          source={require('../../assets/wrong.jpg')}
+          style={styles.wrongCode} />
+      </View>
       <ImageBackground
         source={require('../../assets/bg.png')}
         resizeMode="cover"
@@ -32,22 +38,7 @@ const MainScreen = ({ navigation, route }) => {
             </View>
             <View style={styles.scoreContainer}>
               <View style={styles.scoreBorderContainer}>
-                <View style={[styles.scoreBorderContainer, {
-                  backgroundColor: 'rgba(0,0,0,.3)',
-                  flexDirection: 'row',
-                  justifyContent: "space-evenly",
-                  width: '98%',
-                  height: '85%',
-                  borderWidth: 0,
-                  shadowColor: "#000",
-                  shadowOffset: {
-                    width: 0,
-                    height: 4,
-                  },
-                  shadowOpacity: 0.32,
-                  shadowRadius: 5.46,
-                  elevation: 9,
-                }]}>
+                <View style={[styles.scoreBorderContainer, styles.scoreSubContainer]}>
                   <Text style={styles.score}>{`YOUR SCORE`}</Text>
                   <Text style={styles.score}>{`24240`}</Text>
                 </View>
@@ -56,55 +47,78 @@ const MainScreen = ({ navigation, route }) => {
             <View style={styles.languageContainer}>
               <ImageBackground
                 source={require('../../assets/lang2.png')}
-                style={{
-                  height: '95%', width: '95%', resizeMode: 'contain',
-                }} >
+                style={styles.enContainer} >
                 <Text style={styles.language}>{`EN`}</Text>
               </ImageBackground>
             </View>
 
           </View>
           <View style={styles.helpContainer}>
-            <View style={{
-              height: '90%', backgroundColor: Colors.secondary, width: RFPercentage(6), borderRadius: RFPercentage(1),
-              justifyContent: "center", alignItems: 'center'
-            }}>
-              <Text style={{ fontSize: RFPercentage(2.), fontWeight: '600', color: Colors.black, fontStyle: "italic" }}>{`?`}</Text>
-              <Text style={{ fontSize: RFPercentage(2.), fontWeight: '600', color: Colors.black, fontStyle: "italic" }}>{`Help`}</Text>
+            <View style={styles.helpSubContainer}>
+              <Text style={styles.help}>{`?`}</Text>
+              <Text style={styles.help}>{`Help`}</Text>
             </View>
           </View>
           <View style={styles.msgContainer}>
             <Image
               source={require('../../assets/whatIsCode.png')}
-              style={{ height: '100%', width: '100%', resizeMode: 'contain' }} />
+              style={styles.whatIsCode} />
           </View>
         </View>
         <View style={styles.codeAnswerContainer}>
-          <View style={{ flex: 6, backgroundColor: 'red' }}>
-            <View style={{
-              // height: 100,
-              backgroundColor: 'blue',
-              width: 50
-            }}>
-              <Image
-                source={require('../../assets/numberFrame.png')}
-                style={{ height: '100%', width: '100%', resizeMode: 'contain' }} />
-            </View>
+          <View style={{ flex: 6, justifyContent: "center", flexDirection: 'row' }}>
+            {[3, 4, 9, 7, 4].map((item) => (
+              <View style={styles.codeAnswerSubContainer}>
+                <AntDesign name='caretup' size={RFPercentage(3)} color={Colors.secondary} />
+                <ImageBackground
+                  source={require('../../assets/numberFrame.png')}
+                  style={styles.frameIcon} >
+                  <Text style={[styles.score, { fontSize: RFPercentage(2.6) }]}>{item}</Text>
+                </ImageBackground>
+                <AntDesign name='caretdown' size={RFPercentage(3)} color={Colors.secondary} />
+              </View>
+            ))}
+
           </View>
           <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
             <Image
               source={require('../../assets/check.png')}
-              style={{ height: '80%', width: '80%', resizeMode: 'contain' }} />
+              style={styles.check} />
           </View>
           <View style={{ flex: 2, }}>
             <Image
               source={require('../../assets/hints.png')}
-              style={{ height: '100%', width: '100%', marginLeft: '2%', resizeMode: 'contain' }} />
+              style={styles.hint} />
           </View>
         </View>
-        <View style={styles.codeContainer}></View>
-      </ImageBackground>
-    </View>
+        <View style={[styles.codeContainer, { paddingBottom: RFPercentage(3) }]}>
+          {[0, 0, 0, 0].map(() => (
+            <View style={styles.codeSubContainer}>
+              <View style={styles.frameContainer}>
+                {[0, 0, 0, 0].map(() => (
+                  <View style={{ flex: 1 }}>
+                    <ImageBackground
+                      source={require('../../assets/numberFrame.png')}
+                      resizeMode="contain"
+                      style={styles.frameIcon} >
+                      <Text style={[styles.score, { fontSize: RFPercentage(2.6) }]}>{`3`}</Text>
+                    </ImageBackground>
+                  </View>
+                ))}
+              </View>
+              <View style={{ flex: 1, backgroundColor: Colors.black }}>
+                <ImageBackground
+                  source={require('../../assets/hintframe.png')}
+                  resizeMode="stretch"
+                  style={styles.codeFrame}>
+                  <Text style={[styles.score, styles.resultMsg]}>{`TWO NUMBERS ARE CORRECT BUT WRONGLY PLACED`}</Text>
+                </ImageBackground>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ImageBackground >
+    </View >
   );
 };
 export default MainScreen;
--- src/pages/MainScreen/styles.js
+++ src/pages/MainScreen/styles.js
@@ -22,15 +22,82 @@ export const styles = StyleSheet.create({
     scoreContainer: { flex: 7, justifyContent: "center", alignItems: 'center' },
     scoreBorderContainer: { height: '70%', width: '80%', borderRadius: RFPercentage(1), borderWidth: 1, borderColor: Colors.primary, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' },
     languageContainer: { flex: 1.5, },
-    helpContainer: { flex: 1,  justifyContent:'center',alignItems:"flex-end"},
-    msgContainer: { flex: 1,   },
-    codeAnswerContainer: { flex: 4,   marginHorizontal: RFPercentage(1) },
-    codeContainer: { flex: 4, backgroundColor: 'red', marginHorizontal: RFPercentage(1) },
+    helpContainer: { flex: 1, justifyContent: 'center', alignItems: "flex-end" },
+    msgContainer: { flex: 1, justifyContent: 'center', alignItems: "center" },
+    codeAnswerContainer: { flex: 4, marginHorizontal: RFPercentage(1) },
+    codeContainer: { flex: 4, marginHorizontal: RFPercentage(1) },
     language: {
         fontSize: RFPercentage(2.),
         top: "20%",
         left: '25%',
         fontWeight: '600', color: Colors.secondary, fontStyle: "italic"
     },
-    score: { fontSize: RFPercentage(1.7), fontWeight: 'bold', color: Colors.secondary, fontStyle: "italic" }
+    score: { fontSize: RFPercentage(1.7), fontWeight: 'bold', color: Colors.secondary, fontStyle: "italic" },
+    codeAnswerSubContainer: {
+        width: RFPercentage(7),
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    frameIcon: { height: RFPercentage(8), width: '100%', resizeMode: 'contain', justifyContent: "center", alignItems: 'center' },
+    whatIsCode: {
+        height: '80%', width: '80%', resizeMode: 'contain',
+
+        borderWidth: 0,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+
+    },
+    help: { fontSize: RFPercentage(2.), fontWeight: '600', color: Colors.black, fontStyle: "italic" },
+    helpSubContainer: {
+        height: '90%', backgroundColor: Colors.secondary, width: RFPercentage(6), borderRadius: RFPercentage(1),
+        justifyContent: "center", alignItems: 'center'
+    },
+    enContainer: {
+        height: '95%', width: '95%', resizeMode: 'contain',
+    },
+
+    scoreSubContainer: {
+        backgroundColor: 'rgba(0,0,0,.3)',
+        flexDirection: 'row',
+        justifyContent: "space-evenly",
+        width: '98%',
+        height: '85%',
+        borderWidth: 0,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+    },
+    wrongCode: { height: RFPercentage(20), top: '28%', width: '100%', },
+    wrongCodeContainer: {
+        position: 'absolute', zIndex: 2, height: "100%",
+        paddingBottom: RFPercentage(7),
+        width: '100%', backgroundColor: 'rgba(0,0,0,0.6)'
+    },
+    hint: {
+        height: '100%', width: '100%', marginLeft: '2%'
+        , opacity: .8
+        , resizeMode: 'contain'
+    },
+    codeSubContainer: { flex: 1, borderWidth: 1, marginVertical: RFPercentage(.5), flexDirection: 'row', borderColor: Colors.primary },
+    frameContainer: { flex: 1, flexDirection: 'row' },
+
+
+    codeFrame: {
+        height: '100%', width: '100%', resizeMode: 'stretch',
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    resultMsg: { textAlign: 'center', letterSpacing: RFPercentage(-.15), width: '80%', fontSize: RFPercentage(1.3) },
+    check: { height: '80%', width: '80%', resizeMode: 'contain' },
 });
